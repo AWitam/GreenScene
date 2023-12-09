68774e1b3792997ce2d98f88cf06656e71cc3516
Adrianna Witamborska <32336096+AWitam@users.noreply.github.com>
2023-12-09 20:23:40 +0100

    feat: setup broadcast receiver (#3)
    * feat: add generic SystemBroadcastReceiver composable; change UI on airplane mode change
    
    * feat: observe connectivity status; change UI when no network

---
+++ app/src/main/java/com/example/greenscene/ui/components/AlertBar.kt
@@ -0,0 +1,65 @@
+package com.example.greenscene.ui.components
+
+
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.greenscene.connectivity.ConnectivityObserver
+import com.example.greenscene.connectivity.NetworkConnectivityObserver
+import com.example.greenscene.connectivity.SystemBroadcastReceiver
+import com.example.greenscene.connectivity.isAirplaneModeOn
+
+@Composable
+fun AlertBar(context: Context) {
+    var isAirPlaneMode: Boolean by remember { mutableStateOf(isAirplaneModeOn(context)) }
+
+    SystemBroadcastReceiver(Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+        val isAirplaneModeAfterChange = Settings.Global.getInt(
+            context.contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0
+        ) != 0
+
+        isAirPlaneMode = isAirplaneModeAfterChange
+    }
+
+    lateinit var connectivityObserver: ConnectivityObserver
+    connectivityObserver = NetworkConnectivityObserver(context)
+
+    val networkConnection by connectivityObserver.observe().collectAsState(
+        initial = ConnectivityObserver.Status.Unavailable
+    )
+
+
+    val noConnection =
+        isAirPlaneMode || networkConnection == ConnectivityObserver.Status.Unavailable || networkConnection == ConnectivityObserver.Status.Lost
+
+
+    if (noConnection) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "No Internet Connection",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(colors.error)
+                    .padding(8.dp),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.onError
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/greenscene/ui/views/MapView.kt
+++ app/src/main/java/com/example/greenscene/ui/views/MapView.kt
@@ -1,9 +1,8 @@
-package com.example.greenscene.views
+package com.example.greenscene.ui.views
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -12,11 +11,12 @@ import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
-fun MapView(navController: NavController) {
+fun MapView() {
     val singapore = LatLng(1.35, 103.87)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
