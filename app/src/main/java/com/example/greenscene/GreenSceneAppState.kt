9fe215394dc6c3aafea4e9c873175a3f1b33667e
Adrianna Witamborska <32336096+AWitam@users.noreply.github.com>
2023-12-19 13:32:18 +0100

    feat: splash screen (#7)
    * wip
    
    * fix: hide bottom bar for splash screen
    
    * fix: remove logs; format files

--- app/src/main/java/com/example/greenscene/GreenSceneAppState.kt
+++ app/src/main/java/com/example/greenscene/GreenSceneAppState.kt
@@ -2,12 +2,14 @@ package com.example.greenscene
 
 import android.content.res.Resources
 import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.MutableState
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.CoroutineScope
 
 class GreenSceneAppState(
     val scaffoldState: ScaffoldState,
     val navController: NavHostController,
+    val bottomBarState: MutableState<Boolean>,
     private val resources: Resources,
     coroutineScope: CoroutineScope
 ) {
---
+++ app/src/main/java/com/example/greenscene/ui/screens/splash_screen/SplashViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.greenscene.ui.screens.splash_screen
+
+import androidx.compose.runtime.mutableStateOf
+import com.example.greenscene.MAP_SCREEN
+import com.example.greenscene.SPLASH_SCREEN
+import com.example.greenscene.model.service.AccountService
+import com.example.greenscene.ui.screens.GreenSceneViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val accountService: AccountService
+) : GreenSceneViewModel(){
+    val showError = mutableStateOf(false)
+
+    fun onAppStart(openAndPopUp: (String, String) -> Unit) {
+        showError.value = false
+        if (accountService.hasUser) openAndPopUp(MAP_SCREEN, SPLASH_SCREEN)
+        else createAnonymousAccount(openAndPopUp)
+    }
+
+    private fun createAnonymousAccount(openAndPopUp: (String, String) -> Unit) {
+        launchCatching(snackbar = false) {
+            try {
+                accountService.createAnonymousAccount()
+            } catch (ex: Exception) {
+                showError.value = true
+                throw ex
+            }
+            openAndPopUp(MAP_SCREEN, SPLASH_SCREEN)
+        }
+    }
+
+}
